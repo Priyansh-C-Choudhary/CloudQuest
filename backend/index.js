2dbc71edf5365514a66bce4342dcb0105b227b44
Priyansh Choudhary <77661115+Priyansh-C-Choudhary@users.noreply.github.com>
2024-11-30 14:30:31 +0530

    Docker Compose Done

--- backend/index.js
+++ backend/index.js
@@ -2,34 +2,32 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import path from "path";
 
 import { connectDB } from "./db/connectDB.js";
-
 import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const __dirname = path.resolve();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+// Configure CORS to accept requests from both development and production
+const allowedOrigins = [process.env.FRONTEND_URL || "http://localhost"];
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
 
-app.use(express.json()); // allows us to parse incoming requests:req.body
-app.use(cookieParser()); // allows us to parse incoming cookies
+app.use(express.json()); // Parse incoming JSON requests
+app.use(cookieParser()); // Parse incoming cookies
 
+// API Routes
 app.use("/api/auth", authRoutes);
 
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
-}
-
+// Connect to MongoDB and start the server
 app.listen(PORT, () => {
-	connectDB();
-	console.log("Server is running on port: ", PORT);
+  connectDB();
+  console.log("Server is running on port:", PORT);
 });
